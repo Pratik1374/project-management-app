0447249cbdbbc4b3ca72f52f859cebbbb4d83f42
Pratik <pratik1374@gmail.com>
2024-08-25 22:42:14 +0530

    changed home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,12 +21,13 @@ export default function Home() {
         <meta name="description" content="Manage projects efficiently" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-black">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            <span className="text-[hsl(280,100%,70%)]">Let's start managing</span>
+          <h1 className="text-3xl text-center font-extrabold tracking-tight text-white sm:text-[3rem]">
+            <span className="text-[hsl(280,100%,70%)]">Effortless Task Tracking for Teams.</span>
           </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+          <p className="text-gray-200 text-center max-w-[750px]">Take control of your to-do list. Our intuitive interface makes task management simple, so you can focus on what matters most – getting things done.</p>
+          <div className="flex flex-wrap items-center justify-center gap-4 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
               href="/signup"
