70b88af7a4da936578c6c92f67787b9dcfbe1fd0
Pratik <pratik1374@gmail.com>
2024-08-26 13:14:41 +0530

    added sst configurations for env

--- sst.config.ts
+++ sst.config.ts
@@ -10,7 +10,14 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
+      const site = new NextjsSite(stack, "site", {
+        environment: {
+          DATABASE_URL: process.env.DATABASE_URL!,
+          DIRECT_URL: process.env.DIRECT_URL!,
+          NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET!,
+          NEXTAUTH_URL: process.env.NEXTAUTH_URL!,
+        },
+      });
 
       stack.addOutputs({
         SiteUrl: site.url,
